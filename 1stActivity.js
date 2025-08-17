ba81ced8e665c50a4e2fa085f256bf28ae547561
chrstnczn <christancuizonn123@gmail.com>
2025-08-17 04:00:24 -0700

    1stActivity.js

---
+++ 1stActivity.js
@@ -0,0 +1,28 @@
+var myName = "Christan Cuizon"
+console.log("Name: " ,myName);
+
+let age = "19";
+console.log(age);
+
+const PI = 3.1416;
+console.log(PI);
+
+let isStudent = Number(age) < 25;
+console.log(isStudent);
+
+let person = {
+    firstName: "Christan",
+    middleinitial: "Saturinas",
+    lastname:  "Cuizon",
+address: {
+    barrangay: "R.Castillo",
+    city: "Davao City",
+    province: "Davao del Sur"
+ }
+};
+
+console.log(person.firstName, person.middleinitial, person.lastname);
+console.log(person.address.barrangay, person.address.city, person.address.province);
+
+let colors = ["Blue", "violet", "purple", "yellow", "red"]
+console.log(colors);
